b39c14a71ee597147117c12d3f2419271d6af65a
Andres Aristizabal <andres@vectorized.io>
2020-11-17 17:23:38 -0500

    wapiFeat: feature

--- release.config.js
+++ release.config.js
@@ -2,14 +2,12 @@ module.exports = {
     "branches": ['master'],
     "plugins": [
         ["@semantic-release/commit-analyzer", {
-            "useDefaultReleaseRules": false,
-            "release": false,
             "releaseRules": [
                 // Disable default rules
                 {type: "*", release: false},
                 {type: 'wapiFix', release: 'patch'},
                 {type: 'wapiFeat', release: 'minor'},
-                {type: 'wapiRelease', release: 'patch'},
+                {type: 'wapiRelease', release: 'major'},
             ]
         }],
     ]
