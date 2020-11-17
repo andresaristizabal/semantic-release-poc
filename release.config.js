0fca6a75fe94f281892ccae66159240a10f44ea4
Andres Aristizabal <andres@vectorized.io>
2020-11-16 22:56:21 -0500

    wapiFeat: blac

--- release.config.js
+++ release.config.js
@@ -2,7 +2,35 @@ module.exports = {
     "branches": ['master'],
 
     "plugins": [
-        "@semantic-release/commit-analyzer",
+        ["@semantic-release/commit-analyzer", {
+            "useDefaultReleaseRules": false,
+            "release": false,
+            "releaseRules": [
+                {type: 'wapi-release', "release": 'major'},
+                {type: 'wapi-feat', "release": 'minor'},
+                {type: 'wapi-fix', "release": 'patch'},
+                // Disable default rules
+                {type: "feat", "release": false},
+                {type: "fix", "release": false},
+                {type: "perf", "release": false},
+                {emoji: ':racehorse:', release: false},
+                {emoji: ':bug:', release: false},
+                {emoji: ':penguin:', release: false},
+                {emoji: ':apple:', release: false},
+                {emoji: ':checkered_flag:', release: false},
+                {tag: 'BUGFIX', release: false},
+                {tag: 'FEATURE', release: false},
+                {tag: 'SECURITY', release: false},
+                {tag: 'Breaking', release: false},
+                {tag: 'Fix', release: false},
+                {tag: 'Update', release: false},
+                {tag: 'New', release: false},
+                {component: 'perf', release: false},
+                {component: 'deps', release: false},
+                {type: 'FEAT', release: false},
+                {type: 'FIX', release: false},
+            ]
+        }],
         "@semantic-release/npm",
         ["@semantic-release/exec", {
             "prepareCmd": "echo ${nextRelease.version}",
